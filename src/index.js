c7d1edf914632db9ecd66bacee6278fb1f575b6a
Walid Cherhane <wcherhane17@gmail.com>
2022-04-30 18:53:59 +0000

    🔥 Update To React 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import {render} from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.scss'
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(<App />);
